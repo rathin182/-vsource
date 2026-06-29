db07608426fe16a0493829214f52e388da3c1add
Rathin <rathinb745@gmail.com>
2026-06-29 11:40:57 +0530

    changes

--- app/api/leads/leadsforcounsoler/route.ts
+++ app/api/leads/leadsforcounsoler/route.ts
@@ -27,7 +27,6 @@ export async function GET(req: NextRequest) {
             select: {
               id: true,
               name: true,
-              description: true,
             },
           },
           student: {
